c84569222cfb971f2715770cf01c8b597aa93379
Dmitry Koryakov <dkoryakov0109@gmail.com>
2025-01-01 14:39:36 +0500

    update

--- Fundamentals/01-Fundamentals-Part-1/starter/script.js
+++ Fundamentals/01-Fundamentals-Part-1/starter/script.js
@@ -77,5 +77,81 @@ Cherry`)
 //section 5 conditions same as python instead syntax
 //if () {} else {}
 
-//section 6
+//section 6 coercin conversion
+const inputYear = '1991';
+console.log(inputYear + 18);
+console.log(Number(inputYear) + 18);
+console.log(typeof "John"); //NaN not a number
+// minus => Number ; + => string
+// false values are: '' null und NaN 0
+let q = 4 ;
+let w = '4';
+console.log(q == w, q === w)
+const prmt = prompt('What is going on?')
+console.log(prmt)
 
+//section 7 Logic operators
+// && = and
+// || = or
+const day = "day";
+switch (day) {
+  case 'tuesday':
+    console.log('wrong');
+    break;
+  case "monday":
+    console.log('correct');
+    break;
+  case "day":
+  case "week":
+    console.log('little trick here');
+    break;
+  default:
+    console.log(`Default value`)
+}
+//  if() {}
+//  else if () {}  ---> alternative of case
+
+//challenge
+let DolphinsScore1 = 79;
+let DolphinsScore2 = 178;
+let DolphinsScore3 = 78;
+let KoalasScore1 = 180;
+let KoalasScore2 = 78;
+let KoalasScore3 = 78;
+
+let DolphinsPoints = (DolphinsScore1 + DolphinsScore2 + DolphinsScore3)/3
+let KoalasPoints = (KoalasScore1 + KoalasScore2 + KoalasScore3)/3
+
+console.log(`DolphinsPoints -- ${DolphinsPoints}
+KoalasPoints -- ${KoalasPoints}`)
+
+let winer;
+if (DolphinsPoints > KoalasPoints && (KoalasPoints <100 || DolphinsPoints < 100)) {
+  winer = 'Dolphins';
+  console.log(`Winer is ${winer}`)
+} else if (KoalasPoints > DolphinsPoints && (KoalasPoints <100 || DolphinsPoints < 100)) {
+  winer = 'Koalas';
+  console.log(`Winer is ${winer}`)
+} else if (KoalasPoints === DolphinsPoints) {
+  winer = 'DRAW';
+  console.log(`Winer is ${winer}`)
+} else if (KoalasPoints === DolphinsPoints || (KoalasPoints > 100 && DolphinsPoints > 100)) {
+  winer = 'DRAW > 100';
+  console.log(`Winer is ${winer}`)
+} else {
+  console.log(`Wrong conditions`)
+}
+
+//section 8
+const age = 5;
+age >= 18 ? console.log(`Over 18`) : console.log(`Less than 18`);
+
+const drink = age > 18? "Yes" : "No";
+console.log(drink);
+console.log(`Alternate answer ${age > 18? "Yes" : "No"}`)
+
+//last challenge
+let tip;
+let money = 410;
+money >= 50 && money <= 300 ? tip = 0.2 : tip = 0.15;
+console.log(`tip are ${tip*100}% is equal ${money*tip} money are ${money}`);
